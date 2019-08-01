8e7155a181034a893b6902a76a2e42d065ad5454
dizans <dizansyu@gmail.com>
2019-08-01 14:32:54 +0800

    ch7 expression

---
+++ src/ch7_expressions.rs
@@ -0,0 +1,5 @@
+//There are a few kinds of statements in Rust. 
+// The most common two are declaring a variable binding, 
+// and using a ; with an expression:
+
+// Blocks are expressions too, so they can be used as values in assignments
